d05b4e1ad2d36214184d527433a8f5cf98a5f971
GabrielLasso <gabrielklasso@protonmail.com>
2023-09-06 10:11:25 -0300

    Init koin on android

--- androidApp/src/androidMain/kotlin/com/myapplication/MainActivity.kt
+++ androidApp/src/androidMain/kotlin/com/myapplication/MainActivity.kt
@@ -2,13 +2,14 @@ package com.myapplication
 
 import MainView
 import android.os.Bundle
+import initKoinModule
 import moe.tlaster.precompose.lifecycle.PreComposeActivity
 import moe.tlaster.precompose.lifecycle.setContent
 
 class MainActivity : PreComposeActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        initKoinModule()
         setContent {
             MainView()
         }
